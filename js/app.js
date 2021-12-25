15ce40367488e6c116d0b57003af20946ea5e01d
Lenugo <lnunez@intellisys.com.do>
2021-12-25 12:03:06 -0400

    enable custom amount

--- js/app.js
+++ js/app.js
@@ -1,37 +1,47 @@
 const d = document,
-  bill = d.getElementById('bill-number'),
-  people = d.getElementById('people-number'),
-  tipAmount = Array.from(d.getElementsByClassName('btn-percentage')),
-  btnReset = d.getElementById('btn-reset'),
-  personAmount = d.getElementById('person-amount'),
-  totalAmount = d.getElementById('total-amount'),
-  customAmount = d.getElementById('custom-percentage');
+  bill = d.getElementById("bill-number"),
+  people = d.getElementById("people-number"),
+  tipAmount = Array.from(d.getElementsByClassName("btn-percentage")),
+  btnReset = d.getElementById("btn-reset"),
+  personAmount = d.getElementById("person-amount"),
+  totalAmount = d.getElementById("total-amount"),
+  customAmount = d.getElementById("custom-percentage");
 
-addEventListener('DOMContentLoaded', () => {
+addEventListener("DOMContentLoaded", () => {
   personAmount.innerText = (0).toFixed(2);
   totalAmount.innerText = (0).toFixed(2);
 });
 
 tipAmount.map(tipMap => {
-  tipMap.addEventListener('click', e => {
-    let tip = 0;
-    if (e.target.innerText !== 'Custom') {
-      tip = parseFloat(e.target.innerText);
-    }
+  tipMap.addEventListener("click", e => {
+    let tip = parseFloat(e.target.innerText);
     let convert = tip / 100;
-    const amount = (Number(bill.value) * convert) / Number(people.value);
-    const total = (Number(bill.value) * convert + Number(bill.value)) / Number(people.value);
+    let billValue = bill.value;
+    let peopleValue = people.value;
+    const amount = (Number(billValue) * convert) / Number(peopleValue);
+    const total = (Number(billValue) * convert + Number(billValue)) / Number(peopleValue);
+    console.log(amount);
+    customAmount.addEventListener("input", e => {
+      let value = e.target.value;
+      const inputAmount = (Number(billValue) * (value / 100)) / Number(peopleValue);
+      const inputTotal = (Number(billValue) * (value / 100) + Number(billValue)) / Number(peopleValue);
+      if (!NaN || value.length > 0) {
+        personAmount.innerText = inputAmount.toFixed(2);
+        totalAmount.innerText = inputTotal.toFixed(2);
+      }
+    });
 
-    if (bill.value.length > 0 && people.value.length > 0) {
+    if (billValue.length > 0 && people.value.length > 0) {
       personAmount.innerText = amount.toFixed(2);
       totalAmount.innerText = total.toFixed(2);
     }
   });
 });
 
-btnReset.addEventListener('click', () => {
-  bill.value = '';
-  people.value = '';
+btnReset.addEventListener("click", () => {
+  bill.value = "";
+  people.value = "";
+  customAmount.value = "";
   personAmount.innerText = (0).toFixed(2);
   totalAmount.innerText = (0).toFixed(2);
 });
